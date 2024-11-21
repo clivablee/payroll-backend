51aeec1130f94529e99d6bff9be92a0dbe71f7a9
clivablee <cleeveaarejohn.vidal@gmail.com>
2024-11-21 14:25:50 +0800

    added cors for localhost

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const router = require("./routers/router");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
