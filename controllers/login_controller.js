d7c3fcdbbd6d1673ba254ec7cb12dbc028e8446e
eunhachingu <espajunarjr@gmail.com>
2024-11-21 15:16:49 +0800

    change AuthToken to token

--- controllers/login_controller.js
+++ controllers/login_controller.js
@@ -71,7 +71,7 @@ module.exports = {
 
                     res.cookie("token", token, {   //create cookie
                         httpOnly: true,
-                        sameSite: "Lax",
+                        sameSite: "None",
                     })
                     res.status(200).json({
                         status: "Success",
